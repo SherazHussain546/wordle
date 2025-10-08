113c3a65504c175768401779a5b0d3b0ede7bf86
SYNC TECH Solutions <synctechire@gmail.com>
2025-10-08 16:10:11 +0000

    The link of the official site of wordle is wordle.synctech.ie

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  // This should be your production domain.
-  // For now, we'll build a relative sitemap.
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://daily-word-master.com';
+  const baseUrl = 'https://wordle.synctech.ie';
 
   return [
     {
